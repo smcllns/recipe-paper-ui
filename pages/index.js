11f727be1d057a111fdeb65c3e2a06ed3bf5f418
Sam Collins <oscollins@gmail.com>
2020-06-27 21:18:23 -0700

    Scaffold start of a recipes next app

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Recipes</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <div>
+        <h1>Hellow World, Recipes!</h1>
+      </div>
+    </>
+  );
+};
