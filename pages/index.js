7df0e1c7d09ce8b4497058ef2b7f0977b8ef5d5b
Sam Collins <oscollins@gmail.com>
2020-06-27 21:38:09 -0700

    Queries the api and renders recipes with static build

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,44 @@
 import Head from "next/head";
 
-export default () => {
+const apiUrl = "https://recipe-paper.vercel.app";
+const githubUrl = "https://github.com/smcllns/recipe-paper/tree/master";
+
+export default ({ recipes }) => {
   return (
     <>
       <Head>
         <title>Recipes</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </Head>
-      <div>
-        <h1>Hellow World, Recipes!</h1>
+      <div className="px-2">
+        <h1 className="text-2xl font-bold py-6">Recipes</h1>
+        <ol>
+          {recipes.map((r) => (
+            <li>
+              <Recipe r={r} />
+            </li>
+          ))}
+        </ol>
       </div>
     </>
+  );
+};
+
+export async function getStaticProps(context) {
+  const recipes = await fetch(apiUrl + "/api/v1/recipes").then((r) => r.json());
+
+  return {
+    props: { recipes },
+  };
+}
+
+const Recipe = ({ r }) => {
+  const editSrc = githubUrl + "/" + r.file;
+  const imgSrc = apiUrl + "/" + r.img;
+  return (
+    <p>
+      {/* <img src={imgSrc} /> */}
+      <a href={editSrc}>{r.title}</a>
+    </p>
   );
 };
